f1883b11b8420d8bdf37352c9939a174113470aa
Your Name <ekonge903@gmail.com>
2025-09-30 20:18:18 +0100

    fix(Typescript): Unused variable deletion

--- src/components/landing-page/UserMenuExtensions/ListingUserMenuExtension.tsx
+++ src/components/landing-page/UserMenuExtensions/ListingUserMenuExtension.tsx
@@ -1,25 +1,12 @@
 import React from 'react'
 
 const ListingUserMenuExtension = (): JSX.Element => {
-    const LISTING_ITEMS = [
-        
-        {
-            title: 'Lifestyle and wellnes',
-            category: [
-                {
-                    title: 'Beauty & Personal Care',
-                    description: 'Hair care, Makeup, Salons, Skincare ,spa'
-                }
-            ]
-        }
-    ]
+
   return (
       <div className='mt-8'>
           <h1 className='font-inter text-[32px] font-extrabold'>Browsers Category</h1>
           
-          {/* {LISTING_ITEMS.map((item))=>(
-            <div key={item.}></div>
-          )} */}
+
     </div>
   )
 }
