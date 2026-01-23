cd8df929cb2a931bf0293969525ce94285b793f1
Khalid Oni <onikhalidayo@gmail.com>
2026-01-23 14:24:47 +0100

    update: cange POST to DELETE on sub cancel endpoint

--- src/app/api/payment/subscription/cancel/route.ts
+++ src/app/api/payment/subscription/cancel/route.ts
@@ -1,7 +1,7 @@
 import { forward } from '@/app/api/_lib/backend';
 import { NextRequest, NextResponse } from 'next/server';
 
-// POST /api/payment/subscription/cancel
-export async function POST(req: NextRequest): Promise<NextResponse> {
-  return forward(req, 'POST', '/payment/subscription/cancel/', { auth: true });
+// DELETE /api/payment/subscription/cancel
+export async function DELETE(req: NextRequest): Promise<NextResponse> {
+  return forward(req, 'DELETE', '/payment/subscription/cancel/', { auth: true });
 }
