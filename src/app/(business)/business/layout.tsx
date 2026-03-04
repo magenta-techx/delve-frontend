5b994944ea6879b4f66f4d844a4b30bff8676b32
onikhalid <onikhalidayo@gmail.com>
2026-03-04 12:10:37 +0100

    refactor: Extract layout logic and structure into a new `BusinessLayoutWrapper` component.

--- src/app/(business)/business/layout.tsx
+++ src/app/(business)/business/layout.tsx
@@ -14,29 +14,45 @@ export default function BusinessLayout({
 }: {
   children: React.ReactNode;
 }): React.ReactElement {
-  const { isMobile, isLoading: calculatingScreenWidth } = useIsMobile();
-  const { user } = useUserContext()
-  const { businesses } = useBusinessContext()
+
   return (
     <BusinessProvider>
       <BusinessNotificationsProvider>
-        <div
-          className={cn(
-            'flex h-screen bg-background',
-            isMobile && businesses?.length > 0 ? 'grid !h-dvh grid-rows-[auto,1fr,auto' :
-              isMobile ? 'flex !h-dvh flex-col' : ''
-          )}
-        >
-          {calculatingScreenWidth ? null : isMobile ? <NavbarTop /> : <Sidebar />}
-
-          {/* Main content */}
-          <div className='flex flex-1 flex-col overflow-hidden bg-[#FCFCFD]'>
-            {children}
-          </div>
-
-          {isMobile && user && user.is_brand_owner && businesses?.length > 0 && <NavbarBottom />}
-        </div>
+        <BusinessLayoutWrapper >
+          {children}
+        </BusinessLayoutWrapper>
       </BusinessNotificationsProvider>
     </BusinessProvider>
   );
+}
+
+
+
+const BusinessLayoutWrapper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { isMobile, isLoading: calculatingScreenWidth } = useIsMobile();
+  const { user } = useUserContext()
+  const { businesses } = useBusinessContext()
+
+  return (
+    <div
+      className={cn(
+        'flex h-screen bg-background',
+        isMobile && businesses?.length > 0 ? 'grid !h-dvh grid-rows-[auto,1fr,auto' :
+          isMobile ? 'flex !h-dvh flex-col' : ''
+      )}
+    >
+      {calculatingScreenWidth ? null : isMobile ? <NavbarTop /> : <Sidebar />}
+
+      {/* Main content */}
+      <div className='flex flex-1 flex-col overflow-hidden bg-[#FCFCFD]'>
+        {children}
+      </div>
+
+      {isMobile && user && user.is_brand_owner && businesses?.length > 0 && <NavbarBottom />}
+    </div>
+  )
 }
