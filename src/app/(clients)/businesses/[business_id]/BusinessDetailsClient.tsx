a91b23deee1981609fc438ad289b581203d210c8
onikhalid <onikhalidayo@gmail.com>
2026-05-06 16:49:12 +0100

    update X icon size

--- src/app/(clients)/businesses/[business_id]/BusinessDetailsClient.tsx
+++ src/app/(clients)/businesses/[business_id]/BusinessDetailsClient.tsx
@@ -429,8 +429,8 @@ const BusinessDetailsClient = ({
                           ) : key == 'twitter' ? (
                             <svg
                               xmlns='http://www.w3.org/2000/svg'
-                              width='16'
-                              height='16'
+                              width='24'
+                              height='24'
                               viewBox='0 0 16 16'
                             >
                               <path
